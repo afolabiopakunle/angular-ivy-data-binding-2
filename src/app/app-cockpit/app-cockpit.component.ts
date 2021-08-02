cddce0166491a56abbde0cc467cf121a6f86619b
afolabiopakunle <opakunleafolabi@gmail.com>
2021-08-02 23:14:17 +0100

    Files imported and cleaned up

---
+++ src/app/app-cockpit/app-cockpit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-app-cockpit',
+  templateUrl: './app-cockpit.component.html',
+  styleUrls: ['./app-cockpit.component.css']
+})
+export class AppCockpitComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/app-pcs/app-pcs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-app-pcs',
+  templateUrl: './app-pcs.component.html',
+  styleUrls: ['./app-pcs.component.css']
+})
+export class AppPcsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
